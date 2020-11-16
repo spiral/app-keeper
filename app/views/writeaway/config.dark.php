5252388306f6c2571085a60759c8739b01298766
Valentin V / vvval <valentin@spiralscout.com>
2020-11-16 16:03:35 +0300

    modify writeaway view config

--- app/views/writeaway/config.dark.php
+++ app/views/writeaway/config.dark.php
@@ -1,17 +1,23 @@
 <stack:push name="scripts" unique-id="writeaway">
     <script type="text/javascript" src="/generated/writeaway.js"></script>
     <script type="text/javascript">
+    <?php
+        /**
+         * @var Spiral\Writeaway\Service\MetaProviderInterface $writeawayMetaProvider
+         */
+    ?>
+    @inject($writeawayMetaProvider, \Spiral\Writeaway\Service\MetaProviderInterface::class);
         /**
          * WriteAwayBridge variable is put in global scope in application entry point by front end engineer
          */
         false && WriteAwayBridge.start({
-                imageGalleryUrl: "<?= ('writeaway/imagesList') ?>", // Url to fetch images list
-                getPieceBulkUrl: "<?= ('writeaway/pieceList') ?>", // Url to fetch piece data by multiple ids
-                getPieceUrl: "<?= ('writeaway/getPiece') ?>", // Url to fetch piece data. This is fired only for pieces that can't be read directly from DOM
-                saveMetaUrl: "<?= ('writeaway/saveSeo') ?>", // Url to save SEO data from SEO editor
-                savePieceUrl: "<?= ('writeaway/savePiece') ?>", // Url to save piece. This may be overrided by piece container 'data-save-url' attribute
-                uploadUrl: "<?= ('writeaway/uploadImage') ?>", // Url to upload image resources
-                deleteImageUrl: "<?= ('writeaway/deleteImage') ?>" // Url to delete image
+                imageGalleryUrl: "@route('writeaway:images:list')", // Url to fetch images list
+                uploadUrl: "@route('writeaway:images:upload')", // Url to upload image resources
+                deleteImageUrl: "@route('writeaway:images:delete')" // Url to delete image
+                getPieceUrl: "@route('writeaway:pieces:get')", // Url to fetch piece data. This is fired only for pieces that can't be read directly from DOM
+                savePieceUrl: "@route('writeaway:pieces:save')", // Url to save piece. This may be overrided by piece container 'data-save-url' attribute
+                getPieceBulkUrl: "@route('writeaway:pieces:bulk')", // Url to fetch piece data by multiple ids
+                saveMetaUrl: "unknown to API", // Url to save SEO data from SEO editor
             },
             "<meta></meta>", // Specify HTML for custom meta page headers here for SEO Editor,
             {
@@ -19,10 +25,7 @@
                     // pickerColors: ["red", "blue"]
                 }
             },
-            { // Meta data to attach to pieces when editing them
-                id: "user-id",
-                label: "John Smith",
-            },
+            @json($writeawayMetaProvider->provide()->toArray()),
             typeof window.SpiralSocketConnection !== 'undefined' ? WriteAwayBridge.useWS(window.SpiralSocketConnection) : undefined,
         );
     </script>
