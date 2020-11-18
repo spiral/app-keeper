f99f599211c2b6adc7a5d45364c961538dc898ee
Andrew Kirkovski <andrew@spiralscout.com>
2020-11-18 11:07:00 +0300

    sample fixed

--- app/views/layout/base.dark.php
+++ app/views/layout/base.dark.php
@@ -3,19 +3,17 @@
 <use:bundle path="writeaway/bundle"/>
 
 <head>
-    <writeaway:seo title="${title}" description="${description}" keywords="${description}" id="seo">
+    <writeaway:seo title="${title}" description="${description}" keywords="${keywords}" id="seo">
         <!-- Open Graph / Facebook -->
         <meta property="og:type" content="website">
-        <meta property="og:url" content="https://metatags.io/">
-        <meta property="og:title" content="Meta Tags — Preview, Edit and Generate">
-        <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
+        <meta property="og:title" content="${title}">
+        <meta property="og:description" content="${description}">
         <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
 
         <!-- Twitter -->
         <meta property="twitter:card" content="summary_large_image">
-        <meta property="twitter:url" content="https://metatags.io/">
-        <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate">
-        <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
+        <meta property="twitter:title" content="${title}">
+        <meta property="twitter:description" content="${description}">
         <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
     </writeaway:seo>
     <block:head>
