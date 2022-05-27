c7a2738e374452c01d6395ebc24e2d048a56bce2
Evgeny Barinov <evgeny.barinov@spiralscout.com>
2022-05-27 19:37:32 +0400

    [maintenance]: change Keeper annotations to attributes

--- app/src/Controller/Keeper/ShowcaseController.php
+++ app/src/Controller/Keeper/ShowcaseController.php
@@ -11,86 +11,60 @@ declare(strict_types=1);
 
 namespace App\Controller\Keeper;
 
-use App\Database\User;
-use App\Request\Keeper\User\CreateRequest;
-use App\Request\Keeper\User\RolesRequest;
-use App\Request\Keeper\User\UpdatePasswordRequest;
-use App\Request\Keeper\User\UpdateRequest;
-use App\View\UserGrid;
-use Cycle\ORM\Select;
-use Spiral\DataGrid\Annotation\DataGrid;
-use Spiral\Domain\Annotation\Guarded;
 use Spiral\Keeper\Annotation as Keeper;
-use Spiral\Keeper\Module\RouteRegistry;
 use Spiral\Prototype\Traits\PrototypeTrait;
 
-/**
- * @Keeper\Controller(name="showcase", prefix="/showcase")
- * @Keeper\Sitemap\Group(name="showcase", title="Showcase", options={"icon": "cog"}, position=3.0)
- */
+#[Keeper\Controller(name: 'showcase', prefix: '/showcase')]
+#[Keeper\Sitemap\Group(name: 'showcase', title: 'Showcase', options: ['icon' => 'cog'], position: 3.0)]
 class ShowcaseController
 {
     use PrototypeTrait;
 
-    /**
-     * @Keeper\Action(route="", methods="GET")
-     * @Keeper\Sitemap\Link(title="Short Intro", options={"icon": "eye"})
-     */
-    public function index()
+    #[Keeper\Action(route: '', methods: 'GET')]
+    #[Keeper\Sitemap\Link(title: 'Short Intro', options: ['icon' => 'eye'])]
+    public function index(): string
     {
         return $this->views->render('keeper/showcase/intro');
     }
 
-    /**
-     * @Keeper\Action(route="/forms", methods="GET")
-     * @Keeper\Sitemap\Link(title="Forms", parent="showcase", options={"icon": "list-alt"})
-     */
-    public function showcaseForms()
+    #[Keeper\Action(route: '/forms', methods: 'GET')]
+    #[Keeper\Sitemap\Link(title: 'Forms', parent: 'showcase', options: ['icon' => 'list-alt'])]
+    public function showcaseForms(): string
     {
         return $this->views->render('keeper/showcase/forms');
     }
 
-    /**
-     * @Keeper\Action(route="/autocomplete", methods="GET")
-     * @Keeper\Sitemap\Link(title="Autocomplete", parent="showcase", options={"icon": "i-cursor"})
-     */
-    public function showcaseAutocomplete()
+    #[Keeper\Action(route: '/autocomplete', methods: 'GET')]
+    #[Keeper\Sitemap\Link(title: 'Autocomplete', parent: 'showcase', options: ['icon' => 'i-cursor'])]
+    public function showcaseAutocomplete(): string
     {
         return $this->views->render('keeper/showcase/autocomplete');
     }
 
-    /**
-     * @Keeper\Action(route="/tinymce", methods="GET")
-     * @Keeper\Sitemap\Link(title="Rich Text Editor", parent="showcase", options={"icon": "edit"})
-     */
-    public function showcaseRichText()
+    #[Keeper\Action(route: '/tinymce', methods: 'GET')]
+    #[Keeper\Sitemap\Link(title: 'Rich Text Editor', parent: 'showcase', options: ['icon' => 'edit'])]
+    public function showcaseRichText(): string
     {
         return $this->views->render('keeper/showcase/tinymce');
     }
 
-    /**
-     * @Keeper\Action(route="/datepicker", methods="GET")
-     * @Keeper\Sitemap\Link(title="Datepicker", parent="showcase", options={"icon": "calendar"})
-     */
-    public function showcaseDatepicker()
+    #[Keeper\Action(route: '/datepicker', methods: 'GET')]
+    #[Keeper\Sitemap\Link(title: 'Datepicker', parent: 'showcase', options: ['icon' => 'calendar'])]
+    public function showcaseDatepicker(): string
     {
         return $this->views->render('keeper/showcase/datepicker');
     }
 
-    /**
-     * @Keeper\Action(route="/qrcode", methods="GET")
-     * @Keeper\Sitemap\Link(title="QR Codes", parent="showcase", options={"icon": "qrcode"})
-     */
-    public function showcaseQrCodes()
+    #[Keeper\Action(route: '/qrcode', methods: 'GET')]
+    #[Keeper\Sitemap\Link(title: 'QR Codes', parent: 'showcase', options: ['icon' => 'qrcode'])]
+    public function showcaseQrCodes(): string
     {
         return $this->views->render('keeper/showcase/qrcode');
     }
 
-    /**
-     * @Keeper\Action(route="/datagrid", methods="GET")
-     * @Keeper\Sitemap\Link(title="Datagrids", parent="showcase", options={"icon": "table"})
-     */
-    public function showcaseDataGrids()
+    #[Keeper\Action(route: '/datagrid', methods: 'GET')]
+    #[Keeper\Sitemap\Link(title: 'Datagrids', parent: 'showcase', options: ['icon' => 'table'])]
+    public function showcaseDataGrids(): string
     {
         return $this->views->render('keeper/showcase/datagrids');
     }
