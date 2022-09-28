b3065826764fa2f572bf7ad73b1c2eaffb7783dc
Andrei Kirkouski <andrew@spiralscout.com>
2022-09-28 18:05:11 +0400

    revert changes

--- dockerInit.sh
+++ dockerInit.sh
@@ -4,7 +4,5 @@ docker cp keeper:/var/www/.env . && \
 docker cp keeper:/var/www/vendor . && \
 docker cp keeper-nginx:/usr/share/nginx/html/generated ./public && \
 docker-compose -f docker-compose.yml -f docker-compose-local.yml -f docker-compose-custom-front-local.yml up -d
-docker exec keeper bash -c "php app.php encrypt:key -m .env"
-docker exec keeper bash -c "php app.php configure -vv"
 docker exec keeper bash -c "php app.php migrate:init"
 docker exec keeper bash -c "php app.php migrate"
