4bc92d0c94d5c71d29ed0c6d179da5f761f019fb
Maxim Smakouz <m.smakouz@gmail.com>
2022-09-29 13:39:59 +0300

    Add wait-for-it.sh

--- dockerInit.bat
+++ dockerInit.bat
@@ -5,5 +5,7 @@ docker cp keeper:/var/www/vendor .
 docker cp keeper:/var/www/composer.lock ./composer.lock
 docker cp keeper-nginx:/usr/share/nginx/html/generated ./public
 docker-compose -f docker-compose.yml -f docker-compose-local.yml -f docker-compose-custom-front-local.yml up -d
-docker exec keeper bash -c "php app.php migrate:init"
-docker exec keeper bash -c "php app.php migrate"
+docker exec keeper bash -c "../server/wait-for-it.sh keeper-db:3306 -t 15 -- php app.php encrypt:key -m .env"
+docker exec keeper bash -c "../server/wait-for-it.sh keeper-db:3306 -t 15 -- php app.php configure -vv"
+docker exec keeper bash -c "../server/wait-for-it.sh keeper-db:3306 -t 15 -- php app.php migrate:init"
+docker exec keeper bash -c "../server/wait-for-it.sh keeper-db:3306 -t 15 -- php app.php migrate"
